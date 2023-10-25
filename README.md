2c30b2ffcf6c9c8bdd41a9c5591d9b22ea1957f7
Michael Fortuna <m4tuna@gmail.com>
2023-10-24 20:02:08 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# 🧹 Recursive Cleanup Wizard
+
+A flexible cleanup script to delete specific files or folders recursively based on given criteria. 🗑️
+
+## Requirements:
+
+- **Operating System**: macOS or a Unix-like system.
+- **Shell**: Bash.
+- **Commands**: `find` (typically pre-installed on Unix-like systems).
+- File system support for a `.Trash` directory (for macOS). On other systems, you might need to adjust the script's `move_to_trash` function.
+
+
+## Usage:
+
+To use the script, navigate to the directory where you want to run the cleanup and execute the following commands based on your needs.
+
+- **Run All Checks**:
+  ```bash
+  ~/cleanup.sh ./ --all
+  ```
+
+- **Delete Files with the word "Screenshot"**:
+  ```bash
+  ~/cleanup.sh ./ --name "Screenshot" 
+  ```
+
+- **Delete folders with the same name as a .zip file**:
+  ```bash
+  ~/cleanup.sh ./ --zip 
+  ```
+
+- **Delete files of a specific filetype (e.g., png)**:
+  ```bash
+  ~/cleanup.sh ./ --filetype "png"
+  ```
+
+- **Delete `node_modules` directories**:
+  ```bash
+  ~/cleanup.sh ./ --node_modules
+  ```
+
+**Note**: Always ensure to backup important data and test the script in a safe environment before using it on critical folders. This script does not send items to the Trash, rather it deletes them permanently.
---
+++ cleanup.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Delete files with a given name substring
+delete_files_with_name() {
+    find "$1" -type f -name "*$2*" -exec rm -rf "{}" \;
+}
+
+# Delete folders with the same name as a .zip file in the same directory
+delete_zip_folders() {
+    find "$1" -type f -name '*.zip' | while read -r zipfile; do
+        folder_name="${zipfile%.zip}"
+        if [ -d "$folder_name" ]; then
+            rm -rf "$folder_name"
+        fi
+    done
+}
+
+# Delete files with a given filetype
+delete_filetype() {
+    find "$1" -type f -name "*.$2" -exec rm -rf "{}" \;
+}
+
+# Delete directories by name
+delete_folder_by_name() {
+    find "$1" -type d -name "$2" -exec rm -rf "{}" \;
+}
+
+DIR="$1"
+
+# Process flags
+shift
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --all)
+            delete_files_with_name "$DIR" "Screenshot"
+            delete_zip_folders "$DIR"
+            delete_folder_by_name "$DIR" "node_modules"
+            ;;
+        --name=*)
+            NAME="${1#*=}"
+            delete_files_with_name "$DIR" "$NAME"
+            ;;
+        --zip)
+            delete_zip_folders "$DIR"
+            ;;
+        --filetype=*)
+            TYPE="${1#*=}"
+            delete_filetype "$DIR" "$TYPE"
+            ;;
+        --folderName=*)
+            FOLDER_NAME="${1#*=}"
+            delete_folder_by_name "$DIR" "$FOLDER_NAME"
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
